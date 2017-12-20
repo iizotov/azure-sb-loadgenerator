5f6e832a71de3173fdace9b21427481c23163d10
Igor Izotov <iizotov@users.noreply.github.com>
2017-12-21 10:57:42 +1100

    Add project files.

---
+++ LoadGenerator/CommandLineOptionsClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+
+namespace LoadGenerator
+{
+    class CommandLineOptionsClass
+    {
+        [Option('t', "threads", Required = true,
+            HelpText = "Threads to spawn.", DefaultValue = 5)]
+        public int Threads{ get; set; }
+
+        [Option('s', "size", Required = true,
+            HelpText = "JSON Payload size, real size in bytes = 35 + size", DefaultValue = 1024)]
+        public int MessageSize { get; set; }
+
+        [Option('m', "messagestosend", Required = true,
+            HelpText = "Messages to send in each thread before termination, 0 for infinity", DefaultValue = 100)]
+        public int MessagesToSend { get; set; }
+
+        [Option('c', "connectionstring", Required = true,
+            HelpText = "Event Hub or Service Bus Namespace connection String")]
+        public string ConnectionString { get; set; }
+
+        [Option("name", Required = false,
+            HelpText = "Event Hub or Queue or Topic Name")]
+        public string EHOrQueueOrTopicName { get; set; }
+
+        [Option("checkpoint", Required = false,
+            HelpText = "Checkpoint - log to console every N messages", DefaultValue = 100)]
+        public int Checkpoint { get; set; }
+
+        [Option('b', "batchmode", Required = false,
+            HelpText = "Checkpoint - log to console every N messages", DefaultValue = true)]
+        public bool BatchMode { get; set; }
+
+        [Option("batchsize", Required = false,
+            HelpText = "Checkpoint - log to console every N messages", DefaultValue = 100)]
+        public int BatchSize { get; set; }
+    }
+}
---
+++ LoadGenerator/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace LoadGenerator
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                CommandLineOptionsClass commandLineOptions = new CommandLineOptionsClass();
+                var isValid = CommandLine.Parser.Default.ParseArgumentsStrict(args, commandLineOptions);
+
+                var app = new Program();
+                Task.WaitAll(app.MainAsync(commandLineOptions));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return 1;
+            }
+            Console.WriteLine("Execution Completed");
+            return 0;
+        }
+
+
+        public async Task MainAsync(CommandLineOptionsClass commandLineOptions)
+        {
+            List<Task> tasks = new List<Task>();
+            for (int thread = 0; thread < commandLineOptions.Threads; thread++)
+            {
+                Task t = Task.Run(async () => {
+                    await GenerateLoad(commandLineOptions);
+                });
+                tasks.Add(t);
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private async Task GenerateLoad(CommandLineOptionsClass commandLineOptions)
+        {
+            string utcTimeStamp;
+            string randomPayload;
+            string payload;
+            DateTime start;
+            Int64 messageNumber = 0;
+            BrokeredMessage message;
+            List<BrokeredMessage> messageBatch = new List<BrokeredMessage>();
+
+
+            QueueClient sendClient = QueueClient.CreateFromConnectionString(commandLineOptions.ConnectionString, commandLineOptions.EHOrQueueOrTopicName);
+            Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}, started and connected");
+
+            try
+            {
+                start = DateTime.Now;
+                while (messageNumber < commandLineOptions.MessagesToSend || commandLineOptions.MessagesToSend <= 0)
+                {
+                    utcTimeStamp = ((long)(DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds).ToString();
+                    randomPayload = new Bogus.Randomizer().ClampString("", commandLineOptions.MessageSize, commandLineOptions.MessageSize);
+                    payload = String.Format("{{\"dt\":\"{0}\",\"payload\":\"{1}\"}}", utcTimeStamp, randomPayload);
+                    message = new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(payload)))
+                    {
+                        ContentType = "application/json",
+                        Label = "MyPayload",
+                        TimeToLive = TimeSpan.FromMinutes(100)
+                    };
+                    if(!commandLineOptions.BatchMode)
+                    {
+                        await sendClient.SendAsync(message);
+                        if (messageNumber % commandLineOptions.Checkpoint == 0 && messageNumber > 0)
+                        {
+                            Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}, sent: {messageNumber} / {commandLineOptions.MessagesToSend} messages, message size: {message.Size} bytes, speed: {messageNumber / (DateTime.Now - start).TotalSeconds} msg/sec");
+                        }
+                    }
+                    else
+                    {
+                        messageBatch.Add(message);
+                        if((messageNumber % commandLineOptions.BatchSize == 0 && messageNumber > 0) || 
+                            (messageNumber == (commandLineOptions.MessagesToSend - 1)))
+                        {
+                            await sendClient.SendBatchAsync(messageBatch);
+                            Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}, sent: {messageNumber} / {commandLineOptions.MessagesToSend} messages total, in batches of {commandLineOptions.BatchSize}, message size: {message.Size} bytes, speed: {messageNumber / (DateTime.Now - start).TotalSeconds} msg/sec");
+                            messageBatch.Clear();
+                        }
+                    }
+                    messageNumber++;
+                }
+            }
+            catch
+            {
+                //ignore, keep bombarding!
+            }
+            finally
+            {
+                await sendClient.CloseAsync();
+                Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}, finished");
+            }
+        }
+    }
+
+
+}
